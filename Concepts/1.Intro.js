09f5b2aeb185985ae4b7f353fe43d564c616a2f5
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-20 18:47:51 +0530

    Update 1.Intro.js

--- Concepts/1.Intro.js
+++ Concepts/1.Intro.js
@@ -2,8 +2,8 @@ What is Node JS?
   open-source
   cross-platform
   it runs JS code outside a web browser
-  JS runtime enviornment that runs on V8 engine
-
+  JS runtime enviornment that runs on V8 engine (V8 also written in c++)
+  node js is written in C++
 
 CLI:- (Command Line Interface) - A utility to type commands to your computer rather than clicking.
   e.g Bash on linux, Command prompt on windows, Terminal on Mac
