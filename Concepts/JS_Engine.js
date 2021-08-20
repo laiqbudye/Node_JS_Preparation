dc0e8f3d5e129f7dc31eb83face4dd6b16778c2a
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-20 23:38:27 +0530

    Create JS_Engine.js

---
+++ Concepts/JS_Engine.js
@@ -0,0 +1,22 @@
+JAVASCRIPT vs EcmaScript
+
+most people often gets confused with these two concepts.
+
+Javascript is a actual language. 
+
+How Ecmascript 
+there are so many browser & each browser started to build their own JS engine with some little tweaks or features. here Ecmascript comes in picture which defines
+standards on how the language should work & what features it should have. all browsers need to follow these standards.
+
+in simple language,
+  JS is a language & Ecmascript is a standard on which JS engine is based on.
+  
+  
+  
+V8 Engine
+  is open-source JS engine
+  written in c++ 
+  implements Ecmascript standards
+
+
+
