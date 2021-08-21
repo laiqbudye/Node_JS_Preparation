8b3e117bc62c84988b4111176e0a8bb11b0f4c87
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-21 17:59:24 +0530

    Create 3. Modules.js

---
+++ Concepts/3. Modules.js
@@ -0,0 +1,17 @@
+Module-
+  is a re-usable block of code 
+  
+  Js didnt have this before (there was a workaround but modules were not officially supported)
+
+
+Functions are first class citizens in JS
+  1. We can assign a function to a variable
+  2. We can pass one function as a param to another function 
+  3. We can return a function from a function
+Considering all these, Functions are first class citizens in JS.
+
+
+
+
+
+
