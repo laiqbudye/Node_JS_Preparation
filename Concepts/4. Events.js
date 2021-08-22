6d422cdbac6250329725871e59f1236f1484c134
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-22 13:56:36 +0530

    Create 4. Events.js

---
+++ Concepts/4. Events.js
@@ -0,0 +1,19 @@
+What is Event?
+   something that has happened in our app that we can respond to.
+
+there are two type of events in nodeJs
+  1. System Events 
+        - events coming from computer system like I finished reading a file, a file is open, I have received data from internet & other things like that JS doesnt have.
+        - libuv library provides all these.
+
+  2. Custom Events
+        - they are inside JS core
+        - they are not from libuv library.
+
+
+
+
+
+
+
+
