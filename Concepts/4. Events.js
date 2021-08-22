d987644dd8c0ced57db3eb54a743feff39ddae5d
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-22 15:45:53 +0530

    Update 4. Events.js

--- Concepts/4. Events.js
+++ Concepts/4. Events.js
@@ -17,4 +17,49 @@ Event Listener:-
       - in JS case, the listener will be a function.
       - when an event happens, this function/listener will get invoked.
 
+--------------------------------------------------------------------------------------------
 
+Sample event emitter is as follows:-
+   
+   emitter.js
+
+function Emitter() {
+    this.events = {};
+}
+
+Emitter.prototype.on = function(type, listener){
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+}
+
+Emitter.prototype.emit = function(type) {
+    if(this.events[type]) {
+        this.events[type].forEach(listener => {
+            listener();
+        });
+    }
+}
+
+
+module.exports = Emitter;
+
+
+
+app.js
+
+var Emitter = require('./emitter');
+
+var emtr = new Emitter();
+
+emtr.on('greet', function() {
+    console.log("Somewhere, someone said hello");
+})
+
+
+emtr.on('greet', function() {
+    console.log("A greeting occured!");
+})
+
+console.log("Hello");
+
+emtr.emit('greet');
