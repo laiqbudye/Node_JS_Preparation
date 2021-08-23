25cf14f43ff117c82473376152983472f62f9dea
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-23 19:56:29 +0530

    Create 9. Pipes.js

---
+++ Concepts/9. Pipes.js
@@ -0,0 +1,10 @@
+Pipes
+  connecting two streams by writing to one stream what is being read from another.
+  
+  lets say we have two streams 1. readable 2. writable
+  
+  readble is reading some data & writable is writing some data.
+  
+  so we can read data from readable stream & pass same data to writable stream using PIPE.
+
+
