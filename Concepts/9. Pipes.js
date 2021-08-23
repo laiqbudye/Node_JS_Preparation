fa498d17cebd9da1a2bfae932e1d74f87a8fde9c
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-24 01:18:52 +0530

    Update 9. Pipes.js

--- Concepts/9. Pipes.js
+++ Concepts/9. Pipes.js
@@ -7,4 +7,19 @@ Pipes
   
   so we can read data from readable stream & pass same data to writable stream using PIPE.
 
+  
+  example:- copy contents from one file to another file using pipe.
+    
+    var fs = require
+    
+    var readable = fs.createReadStream(__dirname + '/greet.txt');
+    
+    var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
+    readable.pipe(writable);      // here pipe reads from readable stream & pass it to writable stream. (this way it will copy contents from one file to another)
+    
+      
+we can make chain of pipe also. we only can use pipe command with readable stream
+e.g
+    a.pipe(b)    // here a is readable stream & b is writable
+    a.pipe(b).pipe(c)     // here a is readable, b is duplex(readable/writable) & c is writable
