e513cd1daacd742616f1a85bdacf19446641aa07
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-28 00:24:32 +0530

    Create 13. NPM.js

---
+++ Concepts/13. NPM.js
@@ -0,0 +1,28 @@
+NPM - node pkg manager
+
+pkg mgmt system-
+  software that automates installing & updating pkgs.
+  so whatever pkgs we install or update will be done by this system.
+  
+ 
+Versioning:-
+  specifying what version of this code is
+  e.g 1.1.1  (consider this as current version of app)
+  
+verion is based on this:- Major.Minor.Patch
+
+Patch - when we fix any bug or something we increment this  (1.1.2)
+Minor - when we add small new feature, increment this (1.2.0)
+Major - when we add some big feature, or complete change of look n feel of our app then increment this (2.0.0)
+
+here, number means something which is why it is called as Semantic Versioning.
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
