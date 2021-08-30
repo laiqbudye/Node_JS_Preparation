1a4193e3cc2b41db53f48d28f7e23a9bcc189876
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-31 00:09:09 +0530

    Create 14. express.js

---
+++ Concepts/14. express.js
@@ -0,0 +1,10 @@
+as we have seen previously that we can create a server using node js but express makes it more simple than just a node.
+
+to install express
+  -  npm i express --save
+  
+
+
+
+
+
