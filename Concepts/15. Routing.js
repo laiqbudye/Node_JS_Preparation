c9c50ef416d3827d9a3961cc50f6135e5b7a2340
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-09-01 14:02:44 +0530

    Create 15. Routing.js

---
+++ Concepts/15. Routing.js
@@ -0,0 +1,27 @@
+Routing
+  refers to determining how an application responds to a client request to a particular endpoint.
+  each route can have one or more handler functions, which are executed when the route is matched.
+  by default all requests are get requests. for other type of requests we need to set that request type explicitly. e.g app.post(), app.delete(), app.put(),
+  
+Route examples
+
+var express = requre('express');
+
+var app = express();  
+var port = pocess.env.PORT || 3000;  
+
+app.get('/', function(req, res) {          // URL: localhost:3000/
+  res.json({msg: 'hello wold'});          
+})
+
+
+app.get('/api', function(req, res) {        // URL: localhost:3000/api
+  res.json({firstname: 'Laiq', lastname: 'Budye'});        
+})
+
+app.get('/person/:id', function(req, res) {        // URL: localhost:3000/person/123456
+  res.json({firstname: 'Laiq', lastname: 'Budye'});        
+})
+
+app.listen(port);
+
