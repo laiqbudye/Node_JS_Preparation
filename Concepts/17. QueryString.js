ced17eab2dbe73f14c64acfc67c67eccf635dff2
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-09-02 13:16:19 +0530

    Update 17. QueryString.js

--- Concepts/17. QueryString.js
+++ Concepts/17. QueryString.js
@@ -28,21 +28,45 @@ how to extract data from POST request
 
   to install:- npm install body-parser
  
+   1. bodyParser.urlencoded() - it returns a middleware which only parses urlencoded bodies & populate parsed data on req.body object. (it creates new body obj & adds that on req)
+   
+   2. bodyParser.json() - it returns a middleware which only parses json & populate parsed data on req.body object.
+
     index.html
 
     <body>
       <form method="POST" action="/person">
-        
+        First Name: <input type='text' id='firstname' name='firstname' /> <br />
+        Last Name: <input type='text' id='lastname' name='lastname' /> <br />
+        <input type='submit' value='submit' />
       </form>  
+
+      <!-- sending json data>
+         <script>
+         $.ajax({
+            type: "POST",
+            url: "/personJson",
+            data: JSON.stringify({firstname: 'Jane', lastname: 'doe'}),
+            dataType: 'json',
+            contentType: 'application/json'
+         })
+       </script>
+      <-->
     </body>
       
     var express = requre('express');
     var app = express();  
-    var urlencodedParser = bodyParser.urlencoded({ extended: false });
-  
-    app.get('/person', urlencodedParser, function(req, res) {
-      console.log(req.body.name)       //LAIQ
+    var urlencodedParser = bodyParser.urlencoded({ extended: false });   //extended: false => parsing the URL-encoded data with the querystring library   extended: true => with the qs library
+    var jsonParser = bodyParser.json(); 
+
+    app.post('/person', urlencodedParser, function(req, res) {   // when user click on submit btn this function will get triggered
+      console.log(req.body.firstname)   //LAIQ
       console.log(req.body.lastname)   //BUDYE
+    })
+
+    app.post('/personJson', jsonParser, function(req, res) {   // when user click on submit btn this function will get triggered
+      console.log(req.body.firstname)   //Jane
+      console.log(req.body.lastname)   //doe
     })
     var port = pocess.env.PORT || 3000;    
 
