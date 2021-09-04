95440599d35bb74d1219bf24cd020dde0ab5bdcd
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-09-04 23:42:53 +0530

    Update 1. Intro.js

--- Concepts/1. Intro.js
+++ Concepts/1. Intro.js
@@ -1,9 +1,17 @@
 What is Node JS?
   open-source
   cross-platform
+  single threaded & asynchronous (non-blocking)
   it runs JS code outside a web browser
   JS runtime enviornment that runs on V8 engine (V8 also written in c++)   V8 is responsible to convert Js code to machine code.
   node js is written in C++
+
+
+Node js Features
+  1. Very fast (built on google's V8 engine which is very fast in code execution)
+  2. Single threaded but highly scalable
+  3. No buffering (node js apps never buffers as it always returns data in chunks)
+
 
 CLI:- (Command Line Interface) - A utility to type commands to your computer rather than clicking.
   e.g Bash on linux, Command prompt on windows, Terminal on Mac
@@ -14,3 +22,16 @@ Node Js is a server technology, that is designed to be able to use JS to write s
   
   
 Machine Code:- our code gets converted to machine code which is then read by the processor.
+
+
+node js is non-blocking 
+
+blocking vs non-blocking
+  lets understand this with an example
+     there is waiter & cheff at hotel. whenever any customer comes waiter takes an order & submit it to cheff, then cheff starts working on that.
+     in the meantime if any new customer comes then also waiter takes order & submit it to cheff. (this is non-blocking way)
+     
+     in case of blocking, the waiter will not take order from new customer until current order gets prepared & delivered to the customer.
+     
+     in the same way node also takes multiple requests at a same time, store them in an event loop & process them asynchronously.
+ 
